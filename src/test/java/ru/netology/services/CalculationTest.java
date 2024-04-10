565d6a92370a023296467fe3b43e1b9416b4f8a1
Katya1539 <erkenovakatya@mail.ru>
2024-04-10 22:54:59 +0300

    Parameterized test

--- src/test/java/ru/netology/services/CalculationTest.java
+++ src/test/java/ru/netology/services/CalculationTest.java
@@ -1,26 +1,16 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalculationTest {
-
-    @Test
-    public void test1() {
+    @ParameterizedTest
+    @CsvFileSource(files="scr/test/resources/data.csv")
+    public void test(int expected, int income, int expenses, int threshold) {
         Calculation service = new Calculation();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void test2() {
-        Calculation service = new Calculation();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
