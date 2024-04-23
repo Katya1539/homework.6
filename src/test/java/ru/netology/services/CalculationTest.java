b88c8516071ef25de1d25b1c441b7f0f29977eeb
Katya1539 <erkenovakatya@mail.ru>
2024-04-23 12:54:32 +0300

    Changes

--- src/test/java/ru/netology/services/CalculationTest.java
+++ src/test/java/ru/netology/services/CalculationTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+
 public class CalculationTest {
     @ParameterizedTest
-    @CsvFileSource(files="scr/test/resources/data.csv")
+    @CsvFileSource(files="src/test/resources/data.csv")
     public void test(int expected, int income, int expenses, int threshold) {
         Calculation service = new Calculation();
 
